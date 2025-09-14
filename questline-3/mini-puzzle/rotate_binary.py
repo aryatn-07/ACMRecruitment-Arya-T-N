fb154d7e4aaa9a5bdb14bf1453c0635e635ad141
aryatn-07 <tnarya2007@gmail.com>
2025-09-15 01:06:36 +0530

    Add files via upload

---
+++ questline-3/mini-puzzle/rotate_binary.py
@@ -0,0 +1,13 @@
+def rotate_and_convert(binary_str, k):
+    k = k % len(binary_str)
+    rotated = binary_str[-k:] + binary_str[:-k]
+    decimal_value = int(rotated, 2)
+    return decimal_value
+
+
+binary_str = input('enter binary string:')
+k = 3
+result = rotate_and_convert(binary_str, k)
+print('original:', binary_str)
+print('rotated:', binary_str[-k:] + binary_str[:-k])
+print('decimal value:', result)
